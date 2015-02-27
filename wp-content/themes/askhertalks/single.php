7ca7f14db140cf24f20395bd9ff3bcc9ca16ece0
jawilkin <jawilkin@gmail.com>
2015-02-27 13:10:27 -0500

    Create single blog listing page (single.php)
    - Create single.php based on page.php
    - Copy and paste code containing meta-info about the post from home.php
    into single.php
    - Add WP comments_template
    - In get_sidebar, set parameter to 'blog' so that it displays the
    correct sidebar

--- wp-content/themes/askhertalks/single.php
+++ wp-content/themes/askhertalks/single.php
@@ -4,29 +4,24 @@
 
     <div class="row">
       <div class="col-md-9">
-
-        <div class="page-header">
-            <h1><?php wp_title(''); ?></h1>
-        </div>
         
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-          <article class="post">
-
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            
+          <div class="page-header">
+            <h1><?php the_title(); ?></h1>
             <p><em>
               By <?php the_author(); ?> 
               on <?php echo the_time('l, F jS, Y'); ?> 
               in <?php the_category( ', ' ); ?>. 
               <a href="<?php comments_link(); ?>"><?php comments_number(); ?></a></em>
             </p>
+          </div>
 
-            <?php the_excerpt(); ?>
+          <?php the_content(); ?>
 
-            <hr />
+          <hr>
 
-          </article>
+          <?php comments_template(); ?>
 
         <?php endwhile; else: ?>
 
