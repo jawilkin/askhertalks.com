4ce987232c42de768d49d33c66e4b3c7725b59ef
jawilkin <jawilkin@gmail.com>
2015-03-12 14:59:37 -0400

    Add cover img to jumbotron

--- wp-content/themes/askhertalks/front-page.php
+++ wp-content/themes/askhertalks/front-page.php
@@ -1,32 +1,41 @@
 <?php get_header(); ?>
-
+    
     <div class="jumbotron">
-      <div class="container">
 
-        <img src="<?php bloginfo('stylesheet_directory');?>/images/askher_header.png" class="img-responsive">
+      <!--<div class="container">
+
+        <img src="<?php bloginfo('stylesheet_directory');?>/images/askher_header.png" class="img-responsive hidden-xxs">
+
+        <img src="<?php bloginfo('stylesheet_directory');?>/images/askher_header_sm.png" class="img-responsive visible-xxs">
 
       </div>
+
+      <div class="container">
+        <div class="row" id="front-page-dates">
+          <div class="col-sm-4 city-widget">
+            <div class="city-widget-inner">
+              <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
+            </div>
+          </div>
+          <div class="col-sm-4 city-widget">
+            <div class="city-widget-inner">
+              <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
+            </div>
+         </div>
+          <div class="col-sm-4 city-widget">
+            <div class="city-widget-inner">
+              <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
+            </div>
+          </div>
+        </div>
+      </div>-->
+
+
+
     </div>
+    
 
     <div class="container main-content">
-
-      <div class="row" id="front-page-dates">
-        <div class="col-sm-4 city-widget">
-          <div class="city-widget-inner">
-            <?php if ( dynamic_sidebar( 'front-left' ) ); ?>
-          </div>
-        </div>
-        <div class="col-sm-4 city-widget">
-          <div class="city-widget-inner">
-            <?php if ( dynamic_sidebar( 'front-center' ) ); ?>
-          </div>
-       </div>
-        <div class="col-sm-4 city-widget">
-          <div class="city-widget-inner">
-            <?php if ( dynamic_sidebar( 'front-right' ) ); ?>
-          </div>
-        </div>
-      </div>
 
       <div class="container page-text" id="front-page-text">
         <div class="row">
--- wp-content/themes/askhertalks/header.php
+++ wp-content/themes/askhertalks/header.php
@@ -17,7 +17,7 @@
 
   <body <?php body_class(); ?>>
 
-    <nav class="navbar navbar-fixed-top">
+    <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -26,7 +26,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="<?php bloginfo( 'url'); ?>"><?php bloginfo( 'name'); ?></a>
+          <a class="navbar-brand"><img src="<?php bloginfo('template_directory');?>/images/askher_header_test.png" class="img-responsive"></a>
         </div>
 
         <div id="navbar" class="navbar-collapse collapse navbar-right">
